651d4fa359bd6d468eb0e871331f69ca4e0412b8
Ace <kronicrespawn@gmail.com>
2023-01-12 10:52:11 -0600

    possible knight moves added; start board coor

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// employing a graph breadth-first-search algo for the moves
+const squareRegistry = new Map();
+
+// get and set current cordinates to the board
+const chessSquare = (x , y) => {
+    const xPosition = x;
+    const yPosition = y;
+    let predeccessor;
+
+
+//defined possible knight moves
+const Knight_Moves = [
+        [1, 2], [1, -2], [2 , 1],
+        [2, -1], [-1, 2], [-1 , -2],
+        [-2, 1], [-2, -1]
+    ]
+
+
+}
